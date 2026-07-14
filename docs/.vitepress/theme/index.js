f564974f70a6a8bc1a6a8f411f269bc7bb5479e8
Kuntal Ojha <kuntalojha2021@gmail.com>
2026-07-14 15:30:02 +0530

    remove pdf button

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,14 +1,14 @@
 import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import UnitList from './UnitList.vue'
-import PdfDownload from './PdfDownload.vue'
+// import PdfDownload from './PdfDownload.vue'
 import './custom.css'
 
 export default {
   extends: DefaultTheme,
   Layout() {
     return h(DefaultTheme.Layout, null, {
-      'doc-after': () => h(PdfDownload)
+      // 'doc-after': () => h(PdfDownload)
     })
   },
   enhanceApp({ app }) {
